7d1350c416694c8031473a9d0326ec8f2cd69a27
Bilal Abazaoglu <billabz23@gmail.com>
2026-06-04 09:38:03 +0200

    proje created

---
+++ src/main/java/be/ehb/enterpriseappbilalabazaoglu/EnterpriseAppsBilalAbazaogluApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.enterpriseappbilalabazaoglu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsBilalAbazaogluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAppsBilalAbazaogluApplication.class, args);
+    }
+
+}
