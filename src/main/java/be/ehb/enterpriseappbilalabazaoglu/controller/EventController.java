3b28f3bf372ead16bb7b8a35ef01f62e5fbb9d45
Bilal Abazaoglu <billabz23@gmail.com>
2026-06-10 14:22:09 +0200

    event controller

--- src/main/java/be/ehb/enterpriseappbilalabazaoglu/controller/EventController.java
+++ src/main/java/be/ehb/enterpriseappbilalabazaoglu/controller/EventController.java
@@ -1,11 +1,15 @@
 package be.ehb.enterpriseappbilalabazaoglu.controller;
 
+import be.ehb.enterpriseappbilalabazaoglu.model.Event;
 import be.ehb.enterpriseappbilalabazaoglu.repository.EventRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+
 public class EventController {
     private final EventRepository eventRepository;
 
@@ -15,9 +19,40 @@ public class EventController {
 
     @GetMapping({"/", "/events"})
     public String index(Model model) {
-        model.addAttribute("events", eventRepository.findAll());
-        return "events";
+        model.addAttribute("events", eventRepository.findTop10ByOrderByDateDesc());
+        return "index";
     }
 
+    @GetMapping("/events/new")
+    public String newEvent(Model model) {
+        model.addAttribute("event", new Event());
+        return "events/new";
+    }
+
+    @PostMapping("/new")
+    public String saveEvent(
+            @Valid @ModelAttribute("event") Event event,
+            BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "new";
+        }
+
+        eventRepository.save(event);
+
+        return "redirect:/events";
+    }
+
+    @GetMapping("/id")
+    public String details(@PathVariable Long id, Model model) {
+        Event event = eventRepository.findById(id).orElse(null);
+
+        if (event == null) {
+            return "redirect:/events";
+        }
+        model.addAttribute("event", event);
+
+        return "events/details";
+    }
 
 }
